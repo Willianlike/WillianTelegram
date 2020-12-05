c2a13e92de7b078c644b18045d9a33d3e6fcd55d
Willian <vilyanyaumbaev@gmail.com>
2020-12-05 18:01:52 +0500

    added task 1

---
+++ app/src/main/java/com/williantelegram/UserData.kt
@@ -0,0 +1,26 @@
+package com.williantelegram
+
+class UserData(val id: String, val name: String, val year: String)
+
+enum class UserDataSortType {
+    NAME, YEAR
+}
+
+fun List<UserData>.sorted(byType: UserDataSortType): List<UserData> {
+    return when (byType) {
+        UserDataSortType.NAME ->
+            this.sortedBy { it.name }
+        UserDataSortType.YEAR ->
+            this.sortedBy { it.year }
+    }
+}
+
+class KK {
+    fun gg() {
+        val kk = mutableListOf<UserData>()
+        kk.add(UserData("1", "Me", "321"))
+        kk.add(UserData("2", "You", "123"))
+        println(kk.sorted(UserDataSortType.NAME).map {it.id})
+        println(kk.sorted(UserDataSortType.YEAR).map {it.id})
+    }
+}
